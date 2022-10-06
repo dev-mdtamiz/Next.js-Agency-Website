60cc3a11bd9faadbc83ad23c9663f4a20ec17e00
dev-mdtamiz <tamizuddin.tr@gmail.com>
2022-10-06 14:17:36 +0600

    Uploading old project

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ['img.freepik.com',
+      'firebasestorage.googleapis.com',
+      'i.ibb.co',
+      'www.tailorbrands.com'
+    ],
+  },
+}
